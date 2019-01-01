6c1c02cdc9084943f5f7aa2587ac9edb81c84456
Alex Rael <alprael22@gmail.com>
2018-12-31 18:06:48 -0700

    was able to return the sum of the range of numbers between a and b.

--- test/edu/cnm/deepdive/AddAllTest.java
+++ test/edu/cnm/deepdive/AddAllTest.java
@@ -10,5 +10,13 @@ class AddAllTest {
   void dontGiveMeFive() {
     assertEquals(8, AddAll.dontGiveMeFive(1,9));
     assertEquals(12, AddAll.dontGiveMeFive(4,17));
+    assertEquals(90, AddAll.dontGiveMeFive(1,100));
+  }
+
+  @Test
+  void getSum() {
+    assertEquals(44, AddAll.getSum(2, 9));
+    assertEquals(68, AddAll.getSum(5, 12));
+    assertEquals(-1, AddAll.getSum(0, -1));
   }
 }
